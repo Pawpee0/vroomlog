bb24cbcbfca986e683d425e759758c3304d22b5a
Oscar Sandoval <oscarjs707@gmail.com>
2023-07-08 13:51:18 -0700

    Added database initialization

---
+++ database/index.js
@@ -0,0 +1,42 @@
+const mysql = require('mysql2');
+
+//connect to the database
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'vroomLog'
+});
+
+async function initializeDatabase () {
+  await connection.execute(`CREATE TABLE IF NOT EXISTS Vehicles (
+    id int NOT NULL AUTO_INCREMENT,
+    year int NOT NULL,
+    make text,
+    model text,
+    PRIMARY KEY (id)
+  )`);
+
+  await connection.execute(`CREATE TABLE IF NOT EXISTS MileageEntries (
+    id int NOT NULL AUTO_INCREMENT,
+    carId int NOT NULL,
+    mileage int NOT NULL,
+    dateAdded datetime NOT NULL,
+    dateOccured date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (carId) REFERENCES Vehicles(id)
+  )`);
+
+  await connection.execute(`CREATE TABLE IF NOT EXISTS ServiceEntries (
+    id int NOT NULL AUTO_INCREMENT,
+    carId int NOT NULL,
+    description text,
+    dateAdded datetime NOT NULL,
+    dateOccured date NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (carId) REFERENCES Vehicles(id)
+  )`);
+}
+
+initializeDatabase();
+
+
