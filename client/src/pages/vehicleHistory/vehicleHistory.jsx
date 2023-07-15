a1ee56f79005e8c6813847f0204e8f34b1b36b15
Oscar Sandoval <oscarjs707@gmail.com>
2023-07-15 14:41:52 -0700

    Moved code around files for better organization

--- client/src/pages/vehicleHistory/vehicleHistory.jsx
+++ client/src/pages/vehicleHistory/vehicleHistory.jsx
@@ -1,14 +1,49 @@
 import React from 'react';
+import {useEffect, useState} from 'react';
 
-import {Container} from '@mui/material';
+import {Container, Paper, Stack, Typography} from '@mui/material';
 
-import ServiceHistory from './serviceHistory.jsx';
+import LineChart from '../../components/LineChart/LineChart.jsx';
+
+import axios from 'axios';
 
 
 export default function VehicleHistory (){
+
+  var [vehicleData, setVehicleData] = useState({});
+
+  useEffect(()=>{
+    async function fetchVehicleData(){
+      var response = await axios.get(`${window.location.href}/data`);
+      console.log(response);
+      setVehicleData(response.data);
+    }
+    fetchVehicleData();
+  },[]);
+
   return (
     <Container maxWidth="md">
-      <ServiceHistory/>
+
+      {/* list base */}
+      <Paper sx={{'borderRadius': '25px', 'maxWidth': '100%'}} elevation={1}>
+
+        <Header vehicleData={vehicleData}/>
+        <LineChart id={'graph'} width={500} height={350}/>
+
+      </Paper>
     </Container>
   );
 };
+
+function Header ({vehicleData}) {
+  return (
+    <Stack direction='row' spacing={2} alignItems = 'center' justifyContent='flex-start' sx={{'borderRadius':'25px 25px 0px 0px','backgroundColor': '#212121', 'maxWidth': '100%', 'padding': '20px'}}>
+
+      <Typography variant='h3'>{vehicleData.make}</Typography>
+      <Typography variant='h3'>{vehicleData.model}</Typography>
+
+
+
+    </Stack>
+  );
+}
