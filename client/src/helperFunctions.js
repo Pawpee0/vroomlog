882330a2427d1a57da1a86519e11d0b58ce31441
Oscar Sandoval <oscarjs707@gmail.com>
2023-07-22 16:25:10 -0700

    Added helperFunction file

---
+++ client/src/helperFunctions.js
@@ -0,0 +1,10 @@
+
+var helperFunctions = {};
+
+helperFunctions.formatDateTime = (date)=>{
+  date = date.replace('T', ' ');
+  date = date.replace('Z', '');
+  return date;
+};
+
+module.exports = helperFunctions;
