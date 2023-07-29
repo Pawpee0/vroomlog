e63a6dc387cf328338257381e205ac6399d3108f
Oscar Sandoval <oscarjs707@gmail.com>
2023-07-28 17:13:28 -0700

    VehicleHistory update

--- client/src/pages/vehicleHistory/vehicleHistory.jsx
+++ client/src/pages/vehicleHistory/vehicleHistory.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {useEffect, useState} from 'react';
 
-import {Container, Paper, Stack, Typography} from '@mui/material';
+import {Container, Paper, Stack, Typography, Button} from '@mui/material';
 
 import LineChart from '../../components/LineChart/LineChart.jsx';
+import AddMiles from '../../components/addMiles.jsx';
 
 import axios from 'axios';
 
@@ -11,6 +12,8 @@ import axios from 'axios';
 export default function VehicleHistory (){
 
   var [vehicleData, setVehicleData] = useState({});
+  var [open, setOpen] = useState(false);
+
 
   useEffect(()=>{
     async function fetchVehicleData(){
@@ -27,20 +30,23 @@ export default function VehicleHistory (){
       {/* list base */}
       <Paper sx={{'borderRadius': '25px', 'maxWidth': '100%'}} elevation={1}>
 
-        <Header vehicleData={vehicleData}/>
+        <Header vehicleData={vehicleData} setOpen={setOpen}/>
         <LineChart id={'graph'} width={500} height={350}/>
+        <AddMiles carId={vehicleData.id} open={open} onClose={()=>{setOpen(false)}}/>
+
 
       </Paper>
     </Container>
   );
 };
 
-function Header ({vehicleData}) {
+function Header ({vehicleData, setOpen}) {
   return (
     <Stack direction='row' spacing={2} alignItems = 'center' justifyContent='flex-start' sx={{'borderRadius':'25px 25px 0px 0px','backgroundColor': '#212121', 'maxWidth': '100%', 'padding': '20px'}}>
 
       <Typography variant='h3'>{vehicleData.make}</Typography>
       <Typography variant='h3'>{vehicleData.model}</Typography>
+      <Button variant='contained' onClick={()=>{setOpen(true)}}>Add Miles</Button>
 
 
 
