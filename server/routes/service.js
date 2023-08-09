46ab49066cbcfe483a0a9602985ced5d1521c3d8
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-09 11:04:36 -0700

    Created file for service entry routes

---
+++ server/routes/service.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+const database = require('../../database/index.js');
+const {formatDateTime} = require('../helperFunctions.js');
+
+router.get('', (req, res)=>{});
+
+
+module.exports = router;
