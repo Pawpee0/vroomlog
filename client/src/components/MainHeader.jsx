d3d54eb1ace561d14d3611388d2e5fd48f5ce599
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-11 16:26:11 -0700

    Redid vehicles page without MUI

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-import {Container, Paper, Stack, Typography, Button, Box} from '@mui/material';
 
-export default function MainHeader (){
+export default function MainHeader ({children}){
 
   return (
-    <Paper sx={{'borderRadius': '25px', 'maxWidth': '100%', 'textAlign': 'center', 'borderRadius': '0', 'marginBottom': '10px'}} elevation={1}>
-      <Typography variant='h3'>VroomLog</Typography>
-    </Paper>
+    <>
+      <div id="mainHeader">
+        <h1>VroomLog</h1>
+      </div>
+
+      {children}
+    </>
+
   );
 };
---
+++ client/src/pages/vehicles/list.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import {Card, CardHeader} from '../../components/Card.jsx'
+
+export default function List (){
+  return (
+    <div className='card flexColumn'style={{width: '92vw', position: 'relative', top: '10vh'}}>
+      <Header/>
+      <Body/>
+    </div>
+  );
+};
+
+
+function Header(){
+  return (
+    <div className='cardHeader flexRow'>
+      <h2>My Vehicles</h2>
+      <AddVehicleButton/>
+    </div>
+  )
+}
+
+function Body(){
+  return (
+    <div className='flexColumn'>
+      <Vehicle vehicleData={{make: 'Nissan', model: 'Altima', year: 2012}}/>
+      <Vehicle vehicleData={{make: 'Yamaha', model: 'R3', year: 2015}}/>
+      <Vehicle vehicleData={{make: 'Ford', model: 'F-350', year: 2014}}/>
+    </div>
+  )
+}
+
+function Vehicle({vehicleData}){
+  return (
+    <div className='flexRow cardItem'>
+      <p>{vehicleData.make} {vehicleData.model}</p>
+      <p>{vehicleData.year}</p>
+    </div>
+  )
+}
+
+function AddVehicleButton(){
+  return (
+    <div id='addVehicleButton'>
+      <p>Add Vehicle</p>
+    </div>
+  )
+}
