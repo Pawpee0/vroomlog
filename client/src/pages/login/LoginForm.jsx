a9ea592ec5311b8fcdc39cce43cf10ff5881066c
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-16 23:07:28 -0700

    Added submit button and forgot password link

--- client/src/pages/login/LoginForm.jsx
+++ client/src/pages/login/LoginForm.jsx
@@ -7,9 +7,25 @@ export default function LoginForm (){
       <form class='flexColumn'>
         <TextInput placeholder={'Username'}/>
         <TextInput placeholder={'Password'} type={'password'}/>
+        <Footer/>
       </form>
     </div>
   )
 };
 
+function Footer (){
+  return (
+    <div className='flexRow' style={{'justify-content': 'space-between', 'width': '100%'}}>
+      <p>Forgot Password?</p>
+      <Submit/>
+    </div>
+  )
+}
 
+function Submit () {
+  return (
+    <div className='button'>
+      <p>Submit</p>
+    </div>
+  )
+}
