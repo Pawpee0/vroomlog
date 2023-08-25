543f673021bccb01e8391925495887d40416017f
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-25 15:57:01 -0700

    Environment variables fixed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,11 @@
-import 'dotenv/config';
+
 import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = process.env.FIREBASE_CONFIG;
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
 
 const app = initializeApp(firebaseConfig);
+
+module.exports = {cool: 'qwer'};
+
