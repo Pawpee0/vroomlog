f5e35491dc072e607e6b6e2e03c253973abb87e4
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-25 15:12:24 -0700

    Fixed firebase config file

--- server/firebase.js
+++ server/firebase.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
-const { initializeApp } = require('firebase-admin/app');
+
+const admin = require('firebase-admin');
+
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG);
+const googleCredentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS)
 
 // Initialize Firebase
-const app = initializeApp({
-  credential: process.env.GOOGLE_APPLICATION_CREDENTIALS
+const app = admin.initializeApp({
+  credential: admin.credential.cert(googleCredentials)
 });
+
+module.exports = app;
