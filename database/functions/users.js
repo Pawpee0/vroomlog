176696b2bd47cb7f803c1316f699d1e24e238813
Oscar Sandoval <oscarjs707@gmail.com>
2023-08-27 22:42:51 -0700

    Function to add a user now also fills session field

--- database/functions/users.js
+++ database/functions/users.js
@@ -12,10 +12,11 @@ const database = {};
 database.addUser = function (data){
   return new Promise(async function(fulfill, reject){
     try{
-      await connection.execute(`INSERT INTO Users (id, username) VALUES ("${data.id}", "${data.name}")`)
+      await connection.execute(`INSERT INTO Users (id, username, session) VALUES ("${data.id}", "${data.username}", "${data.session}")`)
       fulfill(200);
     }
     catch(err){
+      console.log(err);
       reject(err);
     }
   });
