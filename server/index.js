9a93af96792a24f6cdd53f1142d71dd3995e0c2f
Oscar Sandoval <oscarjs707@gmail.com>
2023-09-02 15:15:22 -0700

    Moved around server routes so that the session validation middleware doesn't run when a user tries to go to the login page

--- server/index.js
+++ server/index.js
@@ -2,20 +2,51 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.use(bodyParser.json())
-
-app.use((req, res, next)=>{console.log(req.originalUrl); next()})
-
+const auth = require('./authentication.js');
 const users = require('./routes/users.js');
 const miles = require('./routes/miles.js');
 const vehicleData = require('./routes/vehicleData.js');
 const parts = require('./routes/parts.js');
 const maintenance = require('./routes/maintenance.js');
+
+const fireBaseApp = require('./firebase.js');
+const { getAuth } = require('firebase-admin/auth');
+
+
+var sessionVal = function (req, res, next){
+  const sessionCookie = req.cookies.session || '';
+      console.log('cookies', sessionCookie);
+      // Verify the session cookie. In this case an additional check is added to detect
+      // if the user's Firebase session was revoked, user deleted/disabled, etc.
+      getAuth(fireBaseApp)
+        .verifySessionCookie(sessionCookie, true /** checkRevoked */)
+        .then((decodedClaims) => {
+          next();
+        })
+        .catch((error) => {
+          // Session cookie is unavailable or invalid. Force user to login.
+          res.redirect('/login/');
+        });
+};
+
+
+app.use(bodyParser.json())
+app.use(cookieParser());
+app.use((req, res, next)=>{console.log(req.originalUrl); next()})
+
+app.use('/login', express.static(path.join(__dirname, '../client/dist/login')))
+app.use('/', auth);
+
+app.use(sessionVal);
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+
+
+
+
 
 app.use('/users', users);
 app.use('/', miles);
