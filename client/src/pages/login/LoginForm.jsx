c7380d21fdbb708042489acd3171097962e59eeb
Oscar Sandoval <oscarjs707@gmail.com>
2023-09-02 16:26:54 -0700

    Developed login functionality

--- client/src/pages/login/LoginForm.jsx
+++ client/src/pages/login/LoginForm.jsx
@@ -1,31 +1,73 @@
 import React from 'react';
+import {useRef} from 'react';
+
 import TextInput from '../../components/miniComponents/TextInput.jsx';
 
+import auth from '../../firebase.js';
+import {signInWithEmailAndPassword, onAuthStateChanged} from 'firebase/auth';
+
+import axios from 'axios';
+
 export default function LoginForm (){
+
+  var loginData = useRef({});
+
   return (
     <div id='LoginForm' className='form'>
       <form className='flexColumn'>
-        <TextInput placeholder={'Username'}/>
-        <TextInput placeholder={'Password'} type={'password'}/>
-        <Footer/>
+        <TextInput placeholder={'Username'} onChange={(e)=>{loginData.current.Email = e.target.value}}/>
+        <TextInput placeholder={'Password'} type={'password'} onChange={(e)=>{loginData.current.Password = e.target.value}}/>
+        <Footer loginData={loginData}/>
       </form>
     </div>
   )
 };
 
-function Footer (){
+function Footer ({loginData}){
   return (
     <div className='flexRow' style={{'justifyContent': 'space-between', 'width': '100%' , 'margin': '0.3em'}}>
       <a href="google.com">Forgot Password?</a>
-      <Submit/>
+      <Submit loginData={loginData}/>
     </div>
   )
 }
 
-function Submit () {
+function Submit ({loginData}) {
   return (
-    <button type='button' className='submit'>
+    <button type='button' className='submit' onClick={()=>{Login(loginData.current.Email, loginData.current.Password);}}>
       <p>Submit</p>
     </button>
   )
+}
+
+function Login (email, password){
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential)=>{
+    const user = userCredential.user;
+
+    // onAuthStateChanged(auth, (user)=>{
+    //   if (user) {
+    //   }
+    //   else {
+    //     console.log('no user');
+    //   }
+    // });
+
+    //get the IdToken
+    userCredential.user.getIdToken(true)
+    .then((idToken)=>{
+      //grab a session cookie
+      axios.post('/sessionLogin', {
+        idToken: idToken
+      })
+      .then((response)=>{{
+        console.log(response);
+        window.location.replace('http://localhost:3000/vehicles');
+      }});
+    })
+  })
+  .catch((error)=>{
+    console.log(error);
+  });
 }
