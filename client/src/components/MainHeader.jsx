b09a4f99408e4682dd0cd3f18ed3f4a189bbbc7c
Oscar Sandoval <oscarjs707@gmail.com>
2023-09-04 17:39:14 -0700

    Small Ui changes

--- client/src/components/MainHeader.jsx
+++ client/src/components/MainHeader.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
 
+import {signOut} from 'firebase/auth';
+import auth from '../firebase.js';
+
+import axios from 'axios';
 export default function MainHeader ({children}){
 
+  var submitSignOut = ()=>{
+    console.log('running');
+    signOut(auth).then(()=>{
+      //success
+      console.log('success');
+      axios.post('/sessionLogout', {});
+    }).catch((error)=>{
+      console.log(error);
+    })
+  };
   return (
     <>
-      <header id="mainHeader">
+      <header id="mainHeader" className='flexRow'>
         <h1>VroomLog</h1>
+        <button type='button' onClick={submitSignOut}>
+          <p>Sign Out</p>
+        </button>
       </header>
 
       {children}
