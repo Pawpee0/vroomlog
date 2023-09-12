bcba62fc82c222e68d4cf8233276289659525477
Oscar Sandoval <oscarjs707@gmail.com>
2023-09-11 17:55:04 -0700

    Updated modal window to be easier to implement, changed how the modal window closes

--- client/src/components/ModalWindow.jsx
+++ client/src/components/ModalWindow.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export default function ModalWindow ({children, onClose}){
+export default function ModalWindow ({children, setCloseState}){
   return (
-    <div className='modalBase' id='modalBase' onClick={onClose}>
+    <div className='modalBase' id='modalBase' onClick={(e)=>{if(e.target === document.getElementById("modalBase")){setCloseState(false)} }}>
       <div className='card modalWindow flexColumn' style={{width: '40vw'}}>
         {children}
       </div>
