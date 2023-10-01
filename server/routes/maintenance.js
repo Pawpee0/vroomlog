468754e083e63be856f4987a401fa8c807617bbd
Oscar Sandoval <oscarjs707@gmail.com>
2023-10-01 02:01:39 -0700

    Updated server route that adds maintenance entries

--- server/routes/maintenance.js
+++ server/routes/maintenance.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const {formatDateTime} = require('../helperFunctions.js');
 
+const database = require('../../database/functions/maintenance.js');
 
 /*
 {
@@ -12,8 +13,28 @@ const {formatDateTime} = require('../helperFunctions.js');
   partIds: [int]
 }
 */
-router.post('/vehicles/:vehicleId/data/maintenance', (req, res)=>{
+router.post('/vehicles/:id_Vehicles/data/maintenance', (req, res)=>{
 
+  req.body.dateAdded = formatDateTime(req.body.dateAdded);
+  req.body.dateOccured = formatDateTime(req.body.dateOccured);
+
+  database.addMaintenanceEntry(req.body)
+  .then((response)=>{
+    res.send(response);
+  })
+  .catch((err)=>{
+    res.send(err);
+  });
+});
+
+router.get('/vehicles/:id_Vehicles/data/maintenance', (req, res)=>{
+  database.getMaintenanceEntriesByVehicleId(req.params.id_Vehicles)
+  .then((data)=>{
+    res.send(data);
+  })
+  .catch((err)=>{
+    res.send(err);
+  });
 });
 
 
