20d3007b8a26eea6840231781fcce72e5e61c83c
Oscar Sandoval <oscarjs707@gmail.com>
2023-10-01 02:00:08 -0700

    Developed a modal window where users can add a maintenance entry

---
+++ client/src/components/AddMaintenance.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import {useRef} from 'react';
+import ModalWindow from './ModalWindow.jsx';
+
+import axios from 'axios';
+
+export default function AddMaintenance ({id_Vehicles, setCloseState}){
+
+  var data = useRef({id_Vehicles: id_Vehicles});
+
+  var updateData = (e)=>{
+    data.current[e.target.name] = e.target.value;
+  }
+
+  var postData = async ()=>{
+    data.current.dateAdded = new Date;
+    var response = await axios.post(`/vehicles/${id_Vehicles}/data/maintenance`, data.current);
+
+    console.log(response);
+  };
+
+  return (
+    <ModalWindow setCloseState={setCloseState}>
+      <Header/>
+      <Body updateData={updateData}/>
+      <Footer postData={postData}/>
+    </ModalWindow>
+  )
+}
+
+function Header (){
+  return (
+    <div className = 'cardHeader'>
+      <h2>Add Maintenance</h2>
+    </div>
+  )
+}
+
+function Body ({updateData}){
+  return (
+    <form className='flexColumn'>
+      <input name='name' type='text' placeholder='Name' onChange={updateData}></input>
+      <textarea name='description' placeholder='Description' maxlength='250' rows='6' onChange={updateData}></textarea>
+      <input name='mileage' type='text' placeholder='Miles' onChange={updateData}></input>
+      <input name='dateOccured' type='date' placeholder='Date Occured' onChange={updateData}></input>
+    </form>
+  )
+}
+
+function Footer ({postData}){
+  return (
+    <div className='cardFooter'>
+      <button className='green' onClick={postData}>Submit</button>
+    </div>
+  )
+}
+
+
