79a025ef2445a05d3c2366d6c05b243f80f67bfc
Oscar Sandoval <oscarjs707@gmail.com>
2023-10-13 17:15:33 -0700

    /vehicles/:id_Vehicles/data now responds with all the data for the vehicle

--- server/routes/vehicleData.js
+++ server/routes/vehicleData.js
@@ -3,7 +3,9 @@ const router = express.Router();
 const path = require('path');
 
 
-const database = require('../../database/functions/vehicles.js');
+const vehicles = require('../../database/functions/vehicles.js');
+const miles = require('../../database/functions/miles.js');
+const maintenance = require('../../database/functions/maintenance.js');
 
 
 
@@ -11,16 +13,27 @@ router.get('/vehicles/:vehicleId', (req, res)=>{
   res.sendFile(path.join(__dirname, '../../client/dist/vehicleStats/vehicleStats.html'));
 });
 
-router.get('/vehicles/:id_Vehicles/data', (req, res)=>{
+router.get('/vehicles/:id_Vehicles/data', async (req, res)=>{
 
-  database.getVehicleDataById(req.params.id_Vehicles)
-  .then((results)=>{
-    res.send(results[0]);
-  });
+  var id_Vehicles = req.params.id_Vehicles
+
+  Promise.all([
+    vehicles.getVehicleDataById(id_Vehicles),
+    miles.getMileageEntriesByVehicleId(id_Vehicles),
+    maintenance.getMaintenanceEntriesByVehicleId(id_Vehicles)
+  ])
+  .then((values)=>{
+    res.send({
+      ...values[0][0],
+      mileageEntries: values[1],
+      maintenanceEntries: values[2]
+    })
+  })
+
 });
 
 router.delete('/vehicles/:vehicleId/delete', (req, res)=>{
-  database.deleteVehicle(req.params.vehicleId)
+  vehicles.deleteVehicle(req.params.vehicleId)
   .then(()=>{
     res.send('success');
   });
