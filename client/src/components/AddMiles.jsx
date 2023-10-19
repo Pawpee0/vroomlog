efc52a90e46b3e7981f18564537a584d7ed075a4
Oscar Sandoval <oscarjs707@gmail.com>
2023-10-18 19:05:51 -0700

    AddMiles component now validates the data being sent

--- client/src/components/AddMiles.jsx
+++ client/src/components/AddMiles.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {useRef} from 'react';
+import {useState, useRef} from 'react';
 
 import ModalWindow from './ModalWindow.jsx';
+import ErrorBar from './ErrorBar.jsx';
 
 import axios from 'axios';
 
@@ -25,35 +26,48 @@ function Header (){
 
 function Body ({id_Vehicles, setCloseState}){
 
-  var MileageEntry = useRef({
-    id_Vehicles: id_Vehicles,
-    dateAdded: new Date()
-  });
-
-  var updateMileageEntry = (e)=>{
-    MileageEntry.current[e.target.name] = e.target.value;
-  }
+  var [error, setError] = useState('');
 
   var submitMileage = ()=>{
-    //MileageEntry.current.dateOccured = MileageEntry.current.dateOccured.concat('T00:00:00Z');
-    console.log(MileageEntry.current);
-    axios.post(`/vehicles/${id_Vehicles}/data/miles`, MileageEntry.current)
-    .then((response)=>{
-      console.log(response);
-      setCloseState(false);
-      document.location.reload();
-    })
-    .catch((err)=>{
-      console.log(err);
-    });
+    //get data
+    var MileageEntry = {
+      id_Vehicles: id_Vehicles,
+      dateAdded: new Date(),
+      dateOccured: document.getElementById('dateOccuredInput').value,
+      mileage: document.getElementById('mileageInput').value
+    }
+
+    //validate data
+    if (!MileageEntry.mileage){
+      setError('Please enter mileage');
+    } else if (!MileageEntry.dateOccured){
+      setError('Please enter a date');
+    } else {
+      setError('');
+
+      //post data
+      axios.post(`/vehicles/${id_Vehicles}/data/miles`, MileageEntry)
+      .then((response)=>{
+        setCloseState(false);
+        document.location.reload();
+      })
+      .catch((err)=>{
+        console.log(err);
+      });
+
+    }
   };
 
   return (
+    <>
     <form className='flexColumn form'>
-      <input name='dateOccured' type='date' placeholder='Date' max={new Date().toISOString().slice(0,-14)} onChange={updateMileageEntry}></input>
-      <input name='mileage' type='number' pattern="[0-9]*" placeholder='Current Miles' min={1} onChange={updateMileageEntry} autoComplete='off'></input>
+      <input id='dateOccuredInput'name='dateOccured' type='date' placeholder='Date' max={new Date().toISOString().slice(0,-14)} ></input>
+      <input id='mileageInput' name='mileage' type='number' pattern="[0-9]" placeholder='Current Miles' min={1} autoComplete='off' required></input>
       <button type='button' className='green' onClick={submitMileage}>Submit</button>
     </form>
+    {error && <ErrorBar>{error}</ErrorBar>}
+    </>
+
   )
 }
 
