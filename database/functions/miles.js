e2e1cf94922fe0457537c4d791796d686bf66fca
Oscar Sandoval <oscarjs707@gmail.com>
2023-10-27 18:27:24 -0700

    Small bugfix

--- database/functions/miles.js
+++ database/functions/miles.js
@@ -2,18 +2,6 @@ const connection = require('../index.js');
 
 const database = {};
 
-database.getMileageEntriesByVehicleId = function (id_Vehicles) {
-  return new Promise (function(fulfill, reject){
-    connection.execute(`SELECT * FROM MileageEntries WHERE id_Vehicles = ${id_Vehicles} ORDER BY dateOccured`, function(err, results, fields){
-      if (err) {
-        reject (err);
-      } else {
-        fulfill(results);
-      }
-    });
-  });
-}
-
 /*
 {
   id_Vehicles: int,
@@ -35,4 +23,35 @@ database.addMileageEntry = function (id_Vehicles, inputData){
   });
 }
 
+database.getMileageEntriesByVehicleId = function (id_Vehicles) {
+  return new Promise (function(fulfill, reject){
+    connection.execute(`SELECT * FROM MileageEntries WHERE id_Vehicles = ${id_Vehicles} ORDER BY dateOccured`, function(err, results, fields){
+      if (err) {
+        reject (err);
+      } else {
+        fulfill(results);
+      }
+    });
+  });
+}
+
+//checks if the date and mileage is possible based on if the mileage is less than the next entry and more than the last entry
+database.isValidMileage = function (id_Vehicles, date, mileage) {
+  return new Promise (function(fulfill, reject){
+    connection.execute(`SELECT COUNT(*) from MileageEntries WHERE id_Vehicles = ${id_Vehicles} AND
+    ((DATE('${date}') < dateOccured AND ${mileage} < mileage) OR (dateOccured < DATE('${date}') AND mileage < ${mileage}))
+    UNION (SELECT COUNT(*) from MileageEntries WHERE id_Vehicles = ${id_Vehicles});`,
+    function(err, results, fields){
+      if (err) {
+        reject ('Please try again.');
+      } else {
+        if (results.length === 1) {
+          fulfill(true);
+        } else {
+          fulfill(false);
+        }
+      }
+    })
+  });
+}
 module.exports = database;
